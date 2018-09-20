423220d59d428181db814f694d63c5a9156f0960
Алина Белясова <belyasova-alinka@yandex.by>
2018-09-20 10:20:09 +0300

    Use proper gramma

--- First project/First project/Program.cs
+++ First project/First project/Program.cs
@@ -9,7 +9,7 @@ namespace First_project
             string name;
             Console.WriteLine("Enter your name: ");
             name = Console.ReadLine();
-            Console.WriteLine("Hello {0}", name);
+             Console.WriteLine("Hello {0}", name);
             Console.ReadLine();
         }
     }
